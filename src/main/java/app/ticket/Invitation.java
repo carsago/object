cae4b27c55b0100f9d11184024f7b8bc8c1713c4
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-04 19:37:25 +0900

    feat: setup object study

---
+++ src/main/java/app/ticket/Invitation.java
@@ -0,0 +1,7 @@
+package app.ticket;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+    private LocalDateTime when;
+}
---
+++ src/main/java/app/ticket/Ticket.java
@@ -0,0 +1,10 @@
+package app.ticket;
+
+public class Ticket {
+
+    private Long fee;
+
+    public Long getFee() {
+        return fee;
+    }
+}
