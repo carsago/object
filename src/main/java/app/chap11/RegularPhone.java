708159432197dd5dd3222464d39bd4f66b1d40e5
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-23 16:45:23 +0900

    <feat>(chap11.pacakage): chap11 시작

---
+++ src/main/java/app/chap11/RegularPhone.java
@@ -0,0 +1,19 @@
+package app.chap11;
+
+import app.chap2.movie.Money;
+import java.time.Duration;
+
+public class RegularPhone extends Phone {
+    private Money amount;
+    private Duration seconds;
+
+    public RegularPhone(Money amount, Duration seconds) {
+        this.amount = amount;
+        this.seconds = seconds;
+    }
+
+    @Override
+    protected Money calculateCallFee(Call call) {
+        return amount.times(call.getDuration().getSeconds() / seconds.getSeconds());
+    }
+}
