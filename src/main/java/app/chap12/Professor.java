0eb885cc8dc206c98674dd1b92866269b7af4c72
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-27 21:12:37 +0900

    <feat>(chap12/Professor.java): 교수별 성적을 조회하는 클래스

---
+++ src/main/java/app/chap12/Professor.java
@@ -0,0 +1,16 @@
+package app.chap12;
+
+public class Professor {
+    private String name;
+    private Lecture lecture;
+
+    public Professor(String name, Lecture lecture) {
+        this.name = name;
+        this.lecture = lecture;
+    }
+
+    public String compileStatistics() {
+        return String.format("[%s] %s - Avg: %1.f",
+                name, lecture.evaluate(), lecture.average());
+    }
+}
